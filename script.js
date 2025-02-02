d3fb0c3d5bde4715990c6ca7341b8ca47451a044
aria-kara <skartyif@protonmail.com>
2025-02-02 17:08:36 +0100

    fixed pdf displays

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const modal = document.getElementById("pdf-modal")
-    const enlargedPdf = document.getElementById("enlarged-pdf")
-    const closeBtn = document.getElementsByClassName("close-btn")[0]
-    const enlargeBtns = document.getElementsByClassName("enlarge-btn")
-  
-    // Open modal and load PDF
-    Array.from(enlargeBtns).forEach((btn) => {
-      btn.addEventListener("click", function (e) {
-        e.stopPropagation() // Prevent flipcard from flipping back
-        const pdfUrl = this.getAttribute("data-pdf").split("#")[0] // Remove parameters
-        enlargedPdf.src = pdfUrl // Load full PDF with tools
-        modal.style.display = "block"
-      })
+  const modal = document.getElementById("pdf-modal")
+  const enlargedPdf = document.getElementById("enlarged-pdf")
+  const closeBtn = document.getElementsByClassName("close-btn")[0]
+  const enlargeBtns = document.getElementsByClassName("enlarge-btn")
+
+  // Open modal and load PDF
+  Array.from(enlargeBtns).forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.stopPropagation() // Prevent flipcard from flipping back
+      const pdfUrl = this.getAttribute("data-pdf")
+      enlargedPdf.src = pdfUrl // Load full PDF with zoom parameter
+      modal.style.display = "block"
     })
-  
-    // Close modal
-    closeBtn.onclick = () => {
+  })
+
+  // Close modal
+  closeBtn.onclick = () => {
+    modal.style.display = "none"
+  }
+
+  // Close modal if clicking outside of it
+  window.onclick = (event) => {
+    if (event.target == modal) {
       modal.style.display = "none"
     }
-  
-    // Close modal if clicking outside of it
-    window.onclick = (event) => {
-      if (event.target == modal) {
-        modal.style.display = "none"
-      }
-    }
-  })
-  
-  
+  }
+})
+
